52565f64ada77bd6c42a1c5822e3835ed37f9ae6
Colin Lintereur <colin.lintereur@gmail.com>
2020-11-12 16:34:27 -0800

    Committing before udpated LogHours.vue

--- route/hours.js
+++ route/hours.js
@@ -1,4 +1,27 @@
 // what you need for routes
 const express = require('express');
 const router = express.Router();
+
+// logout
+router.post('/', (req, res) => {
+    //Check use logged in
+    if (req.session.user == null) {
+        return res.status(401).json({
+            message: 'Unauthorized',
+        });
+    }
+
+    //Check if hours are empty
+    if (!req.body.timein && !req.body.timeout && !req.body.mealin && !req.body.mealout) {
+        return res.status(422).json({
+            message: 'Please enter your hours',
+        });
+    }
+
+    res.status(200).json({
+        message: 'Hours logged',
+    });
+});
+
+
 module.exports = router;
