3d11f9dde662e2a1a3c3d2b1184ee573330a8f3d
Kody Xu <k3xu@ucsd.edu>
2020-11-20 18:13:41 -0800

    attempted to work on Events

---
+++ frontend/src/components/AddEvents.vue
@@ -0,0 +1,109 @@
+<template>
+<div class="LogHours">
+  <div class="title">
+    <h2>Log Hours</h2>
+    <div class="line"></div>
+  </div>
+  <form @submit.prevent="submit">
+    <div class="form-row">
+      <div class="form-group">
+        <h3>Time In: </h3><input type="time" v-model="timein">
+      </div>
+      <div class="form-group">
+        <h3>Meal In: </h3><input type="time" v-model="mealin">
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group">
+        <h3>Meal Out: </h3><input type="time" v-model="mealout">
+      </div>
+      <div class="form-group">
+        <h3>Time Out: </h3><input type="time" v-model="timeout">
+      </div>
+    </div>
+    <button>Save</button>
+  </form>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Log Hours',
+  data() {
+    return {
+      mealin: null,
+      timein: null,
+      mealout: null,
+      timeout: null,
+    };
+  },
+  created() {
+    // get today's hours
+    // set the variables to those hours
+    const today = new Date();
+    const todayDate = `${today.getFullYear()}-${(today.getMonth() + 1)}-${today.getDate()}`;
+    // console.log(typeof todayDate);
+    let body = {
+      startDate: todayDate,
+      endDate: todayDate,
+    };
+    axios.post('/api/hours/search', body).then((res) => {
+      if (res.data.hour.length === 0) {
+        this.timein = null;
+        this.timeout = null;
+        this.mealin = null;
+        this.mealout = null;
+      } else {
+        this.timein = res.data.hour[0].clock_in;
+        this.timeout = res.data.hour[0].clock_out;
+        this.mealin = res.data.hour[0].meal_in;
+        this.mealout = res.data.hour[0].meal_out;
+      }
+    }).catch((err) => {
+      let message = {
+        message: err,
+        error: true,
+      };
+      this.$store.dispatch('pushNotifications', message);
+    });
+  },
+  methods: {
+    submit() {
+      let body = {
+        mealin: this.mealin,
+        timein: this.timein,
+        mealout: this.mealout,
+        timeout: this.timeout,
+      };
+
+      axios.post('/api/hours/log', body).then((res) => {
+        let message = {
+          message: res.data.message,
+          error: false,
+        };
+
+        this.$store.dispatch('pushNotifications', message);
+        console.log(res.data.message);
+      }).catch((err) => {
+        let message = {
+          message: err.response.data.message,
+          error: true,
+        };
+
+        this.$store.dispatch('pushNotifications', message);
+        console.log(err.response.data.message);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.LogHours{
+  box-shadow: 0 4px 4px rgba(0,0,0,0.1);
+  padding: 20px;
+  background-color:white;
+}
+</style>
--- frontend/src/views/Events.vue
+++ frontend/src/views/Events.vue
@@ -2,14 +2,38 @@
   <div class="events page">
     <h1>Events</h1>
   </div>
+  <AddEvents />
 </template>
 
 <script>
-export default {
+import AddEvents from '../components/AddEvents.vue';
 
+export default {
+  name: 'Events',
+
+  components: {
+    AddEvents,
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+h1{
+  margin-bottom: 2rem;
+  margin-left:2rem;
+}
+
+.grid{
+  width:100%;
+  display:grid;
+  gap: 20px;
+  grid-template-columns:1fr;
+}
+
+@media(min-width: 1024px) {
+  .grid {
+    grid-template-columns:1fr 1fr;
+  }
+}
 </style>
