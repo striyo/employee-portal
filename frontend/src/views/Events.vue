10b489c637e43d1c4becc3916006aa59ce1b5613
k5lin <k5lin@ucsd.edu>
2020-11-20 17:53:31 -0800

    first

--- frontend/src/views/Events.vue
+++ frontend/src/views/Events.vue
@@ -5,11 +5,30 @@
 </template>
 
 <script>
+import AddEvents from '../components/AddEvents.vue'
+
 export default {
 
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+h1{
+  margin-bottom: 2rem;
+  margin-left:2rem;
+}
+
+.grid{
+  width:100%;
+  display:grid;
+  gap: 20px;
+  grid-template-columns:1fr;
+}
+
+@media(min-width: 1024px) {
+  .grid {
+    grid-template-columns:1fr 1fr;
+  }
+}
 </style>
