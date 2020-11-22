6efe4064ff5c5687b4c7a992b10fc1ba69d0b089
DerpyCookie <e2hong@ucsd.edu>
2020-11-21 16:55:35 -0800

    made SearchEventsResults, updated Events.vue

--- frontend/src/views/Events.vue
+++ frontend/src/views/Events.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="events page">
-    <h1>Events</h1>
+    <div class="grid">
+      <div class="left">
+        <AddEvents />
+        <SearchEvents/>
+      </div>
+      <div class="right">
+        <SearchEventsResults />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+// import AddEvents from '../components/AddEvents.vue';
+import SearchEvents from '../components/SearchEvents.vue';
+import SearchEventsResults from '../components/SearchEventsResults.vue';
 
+export default {
+  components: {
+    SearchEvents,
+    SearchEventsResults,
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.grid{
+  width:100%;
+  display:grid;
+  gap: 20px;
+  grid-template-columns:1fr;
+}
 
 </style>
